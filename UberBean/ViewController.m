f9c4d12fa1e1d29a8f05c55255538d53206eed6f
SiHun Sung <sungsihun@gmail.com>
2018-08-17 15:34:34 -0400

    getting the user's location

--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -7,23 +7,49 @@
 //
 
 #import "ViewController.h"
+@import MapKit;
+@import CoreLocation;
 
-@interface ViewController ()
-
+@interface ViewController () <CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) CLLocationManager *locationManager;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
+
+    [self.locationManager requestLocation];
+
+    [self.locationManager startUpdatingLocation];
+
+    [self.mapView setShowsUserLocation:YES];
+    
+    
 }
 
+#pragma mark - CLLocationManagerDelegate
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+    NSLog(@"Moved to %@", locations[0]);
+    
+    MKCoordinateRegion currentRegion = MKCoordinateRegionMake(locations[0].coordinate, MKCoordinateSpanMake(0.1, 0.1));
+    [self.mapView setRegion:currentRegion animated:YES];
+    
 }
 
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"did fail with error: %@", error);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    NSLog(@"Authorization status changed: %d", status);
+}
 
 @end
